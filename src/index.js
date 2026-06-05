15383ed26f54e247dcbfb45084bb401e00ff841c
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2026-06-05 11:44:55 +0100

    final commit before developing dom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { project } from "./project.js";
 let defaultProject = new project('default');
 defaultProject.createTodo('todo one', 'this is a test todo', 'due tomorrow', 'low priority');
 
-console.log(JSON.parse(JSON.stringify(defaultProject)));
+console.log(JSON.parse(JSON.stringify(defaultProject)));çç
 
 defaultProject.deleteTodo(defaultProject.todoList[0].id);
 
