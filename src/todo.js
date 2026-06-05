535733ebfadd56854f2fa8e8ac88607d30a2686a
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2026-06-05 10:54:12 +0100

    added duedate and priority

--- src/todo.js
+++ src/todo.js
@@ -1,7 +1,9 @@
 export class todo {
-    constructor(title, description) {
+    constructor(title, description, dueDate, priority) {
         this.title = title;
         this.desc = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
     }
 
     get title() {
@@ -12,11 +14,27 @@ export class todo {
         return this.desc;
     }
 
+    get priority() {
+        return this.priority;
+    }
+
+    get dueDate() {
+        return this.dueDate;
+    }
+
     set title(newTitle) {
         this.title = newTitle;
     }
 
     set description(newDesc) {
         this.desc = newDesc;
+    }
+
+    set priority(newPriority) {
+        this.priority = newPriority;
+    }
+
+    set dueDate(newDate) {
+        this.dueDate = newDate;
     }
 }
