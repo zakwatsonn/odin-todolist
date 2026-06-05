4830174d40d9c0e4f51f47bf9a2d6c2f506bf402
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2026-06-05 20:09:02 +0100

    create button summons menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import './styles.css'
 import { project } from "./project.js";
 
+let projectList = [];
 let defaultProject = new project('default');
+projectList.push(defaultProject);
+
+//create button logic
+let createButton = document.querySelector(".createButton");
+let createMenu = document.querySelector(".menu");
+
+function openCloseMenu() {
+    createMenu.classList.toggle('open')
+}
+createButton.addEventListener('click', openCloseMenu);
+
+
+
 defaultProject.createTodo('todo one', 'this is a test todo', 'due tomorrow', 'low priority');
-
-console.log(JSON.parse(JSON.stringify(defaultProject)));çç
-
+console.log(JSON.parse(JSON.stringify(defaultProject)));
 defaultProject.deleteTodo(defaultProject.todoList[0].id);
-
 console.log(defaultProject);
