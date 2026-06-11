b9d8780b01824c3ba1d807bb226aed0e51fe8c27
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2026-06-11 21:47:46 +0100

    ugh

--- src/index.js
+++ src/index.js
@@ -2,11 +2,3 @@ import './styles.css'
 import './ui.js'
 import { project } from "./project.js";
 
-let projectList = [];
-let defaultProject = new project('default');
-projectList.push(defaultProject);
-
-defaultProject.createTodo('todo one', 'this is a test todo', 'due tomorrow', 'low priority');
-console.log(JSON.parse(JSON.stringify(defaultProject)));
-defaultProject.deleteTodo(defaultProject.todoList[0].id);
-console.log(defaultProject);
