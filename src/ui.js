9af40b8388586b26411e136a2da6c45b799954a0
zakwatsonn <168912087+zakwatsonn@users.noreply.github.com>
2026-06-11 15:51:08 +0100

    todo create and project create forms open

--- src/ui.js
+++ src/ui.js
@@ -4,11 +4,36 @@ import { project } from './project.js';
 //create button logic
 let createButton = document.querySelector(".createButton");
 let createMenu = document.querySelector(".menu");
+let todoFormSubmit = document.querySelector(".submitButton");
+let todoForm = document.querySelector(".todoFormContainer")
+let todoButton = document.querySelector('.newTodo')
+let projectForm = document.querySelector('.projectFormContainer')
+let projectButton = document.querySelector('.newProject')
 
 function openCloseMenu() {
+    if (projectForm.classList.contains('open')) {
+        projectForm.classList.remove('open');
+    } else if (todoForm.classList.contains('open')) {
+        todoForm.classList.remove('open');
+    }
     createMenu.classList.toggle('open')
 }
 createButton.addEventListener('click', openCloseMenu);
 
 //new todo button logic
-let todoButton = document.querySelector('.newTodo')
+function openCloseTodoForm() {
+    if (projectForm.classList.contains('open')) {
+        projectForm.classList.remove('open');
+    }
+    todoForm.classList.toggle('open')
+}
+todoButton.addEventListener('click', openCloseTodoForm)
+
+//new project button logic
+function openCloseProjectForm() {
+    if (todoForm.classList.contains('open')) {
+        todoForm.classList.remove('open');
+    }
+    projectForm.classList.toggle('open')
+}
+projectButton.addEventListener('click', openCloseProjectForm)
